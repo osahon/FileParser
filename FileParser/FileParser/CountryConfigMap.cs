0b3e1d488d2e4856b8f04565dd7e124dc8a52f3e
Michael Igbinigie <michael.igbinigie@cint.com>
2016-05-02 10:56:00 -0400

    CSVCountryConfig Mapper

---
+++ FileParser/FileParser/CountryConfigMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using FileParser.Models;
+
+namespace FileParser
+{
+	public sealed class CountryConfigMap : CsvClassMap<CountryConfig>
+	{
+		public CountryConfigMap()
+		{
+			Map(m => m.Country).Name("Country");
+			Map(m => m.Year).Name("Year");
+			Map(m => m.Filename).Name("filename");
+			Map(m => m.SexVariable).Name("sex variable");
+			Map(m => m.AgeVarialbe).Name("age variable");
+			Map(m => m.LevelVariable).Name("level variable");
+			Map(m => m.CTOB).Name("CTOB");
+			Map(m => m.OTOB).Name("OTOB");
+			Map(m => m.CSmoker).Name("CSMOKER");
+		}
+	}
+}
